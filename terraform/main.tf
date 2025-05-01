0f3ae41efe5e76c67195083eeb52c6e50960f85f
Daniel Loureiro Cintra <danielloureirocintra@Daniels-MacBook-Air-2.local>
2025-05-01 13:27:03 -0300

    ajuste na terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 resource "aws_s3_bucket" "site_bucket" {
   bucket = "meu-site-magic-search"
   force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "disable_block" {
+  bucket = aws_s3_bucket.site_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
@@ -20,14 +25,16 @@ resource "aws_s3_bucket_website_configuration" "website" {
 }
 
 resource "aws_s3_bucket_policy" "public_access" {
-  bucket = aws_s3_bucket.site_bucket.bucket
+  bucket = aws_s3_bucket.site_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.disable_block]
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
-      Effect = "Allow",
+      Effect    = "Allow",
       Principal = "*",
-      Action = ["s3:GetObject"],
-      Resource = ["${aws_s3_bucket.site_bucket.arn}/*"]
+      Action    = "s3:GetObject",
+      Resource  = "${aws_s3_bucket.site_bucket.arn}/*"
     }]
   })
 }
