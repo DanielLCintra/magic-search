f44043c1694050a20b1e70db4894b6f417350e22
Daniel Loureiro Cintra <danielloureirocintra@Daniels-MacBook-Air-2.local>
2025-05-01 12:21:13 -0300

    ajuste no componente NavBar

--- src/components/Navbar/Navbar.test.tsx
+++ src/components/Navbar/Navbar.test.tsx
@@ -1,6 +1,6 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-import Navbar from "./Navbar";
+import Navbar from "./NavBar";
 
 // Mock do next/image
 jest.mock("next/image", () => (props: any) => {
